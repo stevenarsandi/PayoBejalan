76152f4dfa0ea319f1a9d58fbca1498a7cc64b88
stevenarsandi <arsandisteven@gmail.com>
2023-03-08 18:41:15 +0700

    tampilan dan sedikit database

---
+++ app/src/main/java/com/si61/myapplication/UbahActivity.java
@@ -0,0 +1,14 @@
+package com.si61.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UbahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
+}
