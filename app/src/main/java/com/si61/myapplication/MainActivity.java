d4f259aa1a8406ddb0a58093a3007378f1211908
StevenArsandi <arsandisteven@gmail.com>
2023-03-09 18:34:13 +0700

    lanjutan

--- app/src/main/java/com/si61/myapplication/MainActivity.java
+++ app/src/main/java/com/si61/myapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package com.si61.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fabTambah;
+    private RecyclerView rvDestinasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabTambah = findViewById(R.id.fab_tambah);
+        rvDestinasi = findViewById(R.id.tv_destinasi);
+
+        fabTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TambahActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/si61/myapplication/MyDatabaseHelper.java
+++ app/src/main/java/com/si61/myapplication/MyDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.si61.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,17 +8,49 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
-    public MyDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private Context ctx;
+    private static final String DATABASE_NAME = "db_payo_bejalan";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "tbl_destinasi";
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_NAMA = "nama";
+    private static final String FIELD_ALAMAT = "alamat";
+    private static final String FIELD_JAM = "jam";
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.ctx = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME + "(" +
+                FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                FIELD_NAMA + " VARCHAR(50), " +
+                FIELD_ALAMAT + "TEXT, " +
+                FIELD_JAM + " VARCHAR(30)" +
+                ");"
+                ;
 
+        db.execSQL(query);
+        onCreate(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
 
+    public long tambahData(String nama, String alamat, String jam) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(FIELD_NAMA, nama);
+        cv.put(FIELD_ALAMAT, alamat);
+        cv.put(FIELD_JAM, jam);
+
+        long eksekusi = db.insert(TABLE_NAME, null, cv);
+        return eksekusi;
     }
 }
